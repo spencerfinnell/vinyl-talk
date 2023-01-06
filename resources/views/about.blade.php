13faced922bea75069b3f6d56598ddff89fa1041
adamjohnlea <imnogeekdptcom@me.com>
2023-01-06 14:00:44 -0700

    learn basics of components, layouts, and more on views

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Vinyl Talk | About</title>
-</head>
-<body>
-    <h1>What is Vinyl Talk all about?</h1>
-</body>
-</html>
+<x-layout>
+
+    <h1>About us!</h1>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\SinglePostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,4 @@ use App\Http\Controllers\AboutController;
 
 Route::get('/', [HomeController::class, "home"]);
 Route::get('/about', [AboutController::class, "about"]);
+Route::get('/single-post', [SinglePostController::class, "post"]);
