6ee618d8f5e5980d650276fe37950786dec3ee61
adamjohnlea <imnogeekdptcom@me.com>
2023-01-06 13:27:10 -0700

    learn basics of controllers and views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/about', function () {
-   return view('about');
-});
+Route::get('/', [HomeController::class, "home"]);
+Route::get('/about', [AboutController::class, "about"]);
