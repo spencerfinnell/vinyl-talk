fe27204032c5681429858748609e3a6c58fe1a7e
adamjohnlea <imnogeekdptcom@me.com>
2023-01-20 22:56:18 -0700

    add auth and guest middleware

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [UserController::class, "showCorrectHomepage"]);
+Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login');
 
-Route::get('/create-post', [PostController::class, "showCreateForm"]);
-Route::post('/create-post', [PostController::class, "storeNewPost"]);
+Route::get('/create-post', [PostController::class, "showCreateForm"])->middleware('auth');
+Route::post('/create-post', [PostController::class, "storeNewPost"])->middleware('auth');
 Route::get('/post/{post}', [PostController::class, "showSinglePost"]);
 
-Route::post('/register', [UserController::class, "register"]);
-Route::post('/login', [UserController::class, "login"]);
-Route::post('/logout', [UserController::class, "logout"]);
+Route::post('/register', [UserController::class, "register"])->middleware('guest');
+Route::post('/login', [UserController::class, "login"])->middleware('guest');
+Route::post('/logout', [UserController::class, "logout"])->middleware('auth');
