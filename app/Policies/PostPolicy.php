b8cd840f7e7c89b07f086da6c6ce6332485b43ef
adamjohnlea <imnogeekdptcom@me.com>
2023-01-21 11:59:19 -0700

    add moderator permissions

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -53,6 +53,9 @@ class PostPolicy
      */
     public function update(User $user, Post $post)
     {
+        if ($user->isAdmin == 1) {
+            return true;
+        }
         return $user->id === $post->user_id;
     }
 
@@ -65,6 +68,9 @@ class PostPolicy
      */
     public function delete(User $user, Post $post)
     {
+        if ($user->isAdmin == 1) {
+            return true;
+        }
         return $user->id === $post->user_id;
     }
 
